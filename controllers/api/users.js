66ebf540142cd6767390f34179a64c5fcdb9aad4
Willlasbury <asburyw229@gmail.com>
2023-05-28 14:27:42 -0700

    added checkes and responses to user routes '

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,35 +1,53 @@
 const router = require("express").Router();
 const User = require("../../models");
 
-router.get("/:id", async (req, res) => {
+// Get all users
+router.get("/", async (req, res) => {
   try {
-    const userId = req.params.id;
-    const dbData = await User.findByPk(userId)
-    const user = await dbData.get({plain:true})
-    if (user) {
-        res.json(user)
+    const dbData = await User.findAll();
+
+    if (dbData.length === 0) {
+      return res.status(404).json({ msg: "no Users in database!" });
     }
+      return res.json(dbData);
+    
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ msg: "some error", err: err });
+    return res.status(500).json({ msg: "could not get users", err: err });
   }
 });
 
-router.post("/", async (req, res) => {
-    try {
-      const newUser = {
-        username: req.body.userName,
-        password: req.body.password,
-      }
-      const dbData = await User.create(newUser)
-      
-      if (dbData) {
-          res.json(dbData)
-      }
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ msg: "some error", err: err });
+// Get user by id
+router.get("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const dbData = await User.findByPk(userId);
+
+    if (!dbData) {
+      return res.status(404).json({ msg: "User not found!" });
     }
+    return res.json(dbData);
+
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "could not get user", err: err });
+  }
+});
+
+// Create user
+router.post("/", async (req, res) => {
+  try {
+    const newUser = {
+      username: req.body.userName,
+      password: req.body.password,
+    };
+    const dbData = await User.create(newUser);
+  
+    return res.json(dbData)
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Could not create user", err: err });
+  }
 });
 
 module.exports = router;
