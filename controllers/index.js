ee44dcd88cf3df51389ce8ffef2c1d653ff7008f
Willlasbury <asburyw229@gmail.com>
2023-05-29 10:03:42 -0700

    added test socket code to root index

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 const api = require("./api");
 router.use("/api", api);
 
+const chat = require('./socket/chat')
+router.use('/chat', chat)
+
 // router.get("/", (req, res) => res.json("home"));
 
 module.exports = router;
--- index.js
+++ index.js
@@ -7,9 +7,39 @@ const sequelize = require('./config');
 const cors = require("cors")
 
 // Sets up the Express App
-// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
+// =============================================================
+
+// set up socket.io
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server({
+    cors: {
+      origin: `http://localhost:${PORT}`
+    }
+  })
+  
+
+console.log('===\n\n\ntest\n\n\n===')
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+    socket.join('clock-room')
+  
+  socket.on('disconnect',(reason)=>{
+    console.log(reason)
+  })
+  });
+
+  setInterval(()=>{
+    io.to('clock-room').emit('time', new Date())
+},1000)
+console.log('===\n\n\ntest\n\n\n===')
+// =============================================================
+
+
+
 
 // Requiring our models for syncing
 const { User} = require('./models');
