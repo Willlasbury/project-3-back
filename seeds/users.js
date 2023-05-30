a4e4e5da876f1c115884a0987e6fb888da4f8ed0
Willlasbury <asburyw229@gmail.com>
2023-05-30 15:55:55 -0700

    added user seed data

--- seeds/users.js
+++ seeds/users.js
@@ -1,7 +1,63 @@
-users = {
-    username: 'Chris',
-    password: 'password',
-    email: 'chris@chris.chris',
-}
-
-module.exports = users
+users = [
+  {
+    username: "Chris",
+    password: "password",
+    email: "chris@chris.chris",
+    seller_rating: 1,
+  },
+  {
+    username: "Will",
+    password: "password",
+    email: "Will@Will.Will",
+    seller_rating: 3,
+  },
+  {
+    username: "Bob",
+    password: "password",
+    email: "Bob@Bob.Bob",
+    seller_rating: 1,
+  },
+  {
+    username: "Hailey",
+    password: "password",
+    email: "Hailey@Hailey.Hailey",
+    seller_rating: 1,
+  },
+  {
+    username: "Molly",
+    password: "password",
+    email: "Molly@Molly.Molly",
+    seller_rating: 1,
+  },
+  {
+    username: "joe",
+    password: "password",
+    email: "joe@joe.joe",
+    seller_rating: 1,
+  },
+  {
+    username: "Nat",
+    password: "password",
+    email: "Nat@Nat.Nat",
+    seller_rating: 1,
+  },
+  {
+    username: "Liv",
+    password: "password",
+    email: "Liv@Liv.Liv",
+    seller_rating: 1,
+  },
+  {
+    username: "steve",
+    password: "password",
+    email: "steve@steve.steve",
+    seller_rating: 1,
+  },
+  {
+    username: "last",
+    password: "password",
+    email: "last@last.last",
+    seller_rating: 4,
+  },
+];
+module.exports = users;
