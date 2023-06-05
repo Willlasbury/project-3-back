9c49f2f0e569b1b23e8e73976fd038f44a085ddd
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-06-05 12:41:46 -0700

    added photos route

--- seeds/photos.js
+++ seeds/photos.js
@@ -1,14 +1,14 @@
 const photos = [
-  { url: "https://placekitten.com/200/300", ItemId: "1" },
-  { url: "https://placekitten.com/200/200", ItemId: "2" },
-  { url: "https://placekitten.com/200/500", ItemId: "3" },
-  { url: "https://placekitten.com/400/600", ItemId: "1" },
-  { url: "https://placekitten.com/400/400", ItemId: "1" },
-  { url: "https://placekitten.com/300/500", ItemId: "1" },
-  { url: "https://placekitten.com/300/800", ItemId: "1" },
-  { url: "https://placekitten.com/500/800", ItemId: "1" },
-  { url: "https://placekitten.com/600/800", ItemId: "1" },
-  { url: "https://placekitten.com/800/800", ItemId: "1" },
+  { url: "https://placekitten.com/200/300", item_id: "1" },
+  { url: "https://placekitten.com/200/200", item_id: "2" },
+  { url: "https://placekitten.com/200/500", item_id: "3" },
+  { url: "https://placekitten.com/400/600", item_id: "1" },
+  { url: "https://placekitten.com/400/400", item_id: "1" },
+  { url: "https://placekitten.com/300/500", item_id: "1" },
+  { url: "https://placekitten.com/300/800", item_id: "1" },
+  { url: "https://placekitten.com/500/800", item_id: "1" },
+  { url: "https://placekitten.com/600/800", item_id: "1" },
+  { url: "https://placekitten.com/800/800", item_id: "1" },
 ];
 
 module.exports = photos;
