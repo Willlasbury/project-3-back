42fb938a8885cc4650ca375d59fa9c1ffb176ba1
Michael Campbell <michaelcampbell@Michaels-MBP.domain>
2023-06-06 11:35:11 -0700

    added foreign keys on item creation

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -65,6 +65,8 @@ router.post("/", async (req, res) => {
     };
     //if want to add multiple would have to make a bulk create where urls are added into an array and then mapping over it. Include an array in the request body and if there are multiple photos do a bulk
     const dbData = await Item.create(newItem);
+    dbData.setSeller(user);
+    dbData.setCategory(category);
     const photoArr = [];
     const newPhoto = {
       url: req.body.url,
@@ -80,8 +82,8 @@ router.post("/", async (req, res) => {
       photo.setItem(dbData);
     });
 
-    // const dbPhotoData = await Photo.create(newPhoto);
-    // dbData.setCategory(category);
+    const dbPhotoData = await Photo.create(newPhoto);
+    dbData.setCategory(category);
 
     return res.json({
       item: dbData,
