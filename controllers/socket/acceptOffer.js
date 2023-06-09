91645b366346ce767ab05f5c90b4da857a0ae01d
Willlasbury <asburyw229@gmail.com>
2023-06-09 13:06:51 -0700

    building message to user

--- controllers/socket/acceptOffer.js
+++ controllers/socket/acceptOffer.js
@@ -1,8 +1,9 @@
-const {Item, Offer} = require('../../models')
+const {Item, Offer, User} = require('../../models')
 
 async function acceptOffer(data, users, socket, io) {
   try {
-    // adjust item info in db
+console.log("1 data:", data)  
+  // adjust item info in db
     const itemUpdate = {
       sold_status: true,
       buyer_id: data.offer.offerer_id,
@@ -16,9 +17,21 @@ async function acceptOffer(data, users, socket, io) {
       { accepted: true },
       { where: { id: data.offer.id } }
     );
-    console.log("res:", res);
 
     // send notification to offerer
+    // create message in db 
+    // message: Hello user(data.offer.Offerer_id), other user(data.offer.Item.seller_id) has accepted your offer for item (data.offer.item.title)
+    // fetch users names and item name
+    const buyer = await User.findByPk(data.offer.offerer_id)
+    const seller = await User.findByPk(data.offer.Item.seller_id)
+    const messageItem = item.title
+    console.log("item:", item)
+    
+    const message = `Hello ${buyer.userName}. ${seller.userName} has accepted your offer for ${item}`  
+    console.log('===\n\n\ntest\n\n\n===')
+    console.log("message:", message)
+    console.log('===\n\n\ntest\n\n\n===')
+
     // io.to(users[data.offer.offerer_id]).emit("done_deal");
     socket.emit("accept_res");
   } catch (error) {
